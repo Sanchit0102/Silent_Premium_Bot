9d5f66f376e62b142736cf0e31cc5444251461c1
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-05-04 20:18:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# (c) 𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧
+import os 
+from os import getenv, environ
+
+API_ID = int(getenv('API_ID', 25833520))
+API_HASH = str(getenv('API_HASH', '7d012a6cbfabc2d0436d7a09d8362af7'))
+BOT_TOKEN = str(getenv('BOT_TOKEN' , '6738059809:AAFfYft25qo-iKP-XpgLgq9ZCyhKoew8994'))
